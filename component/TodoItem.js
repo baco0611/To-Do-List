20a0b36c612fbdeaf82989f626a2b2ea19aca792
baco0611 <baoblink@gmail.com>
2023-03-31 21:25:09 +0700

    complete the clone

--- component/TodoItem.js
+++ component/TodoItem.js
@@ -1,17 +1,32 @@
 import html from "../core.js";
+import { connect } from "../store.js";
 
 
-function TodoItem() {
+function TodoItem({ todo, index, editIndex }) {
     return html`
-        <li class="">
+        <li 
+            class="${todo.completed && 'completed'} 
+            ${index === editIndex && 'editing'}"
+        >
             <div class="view">
-                <input class="toggle" type="checkbox" checked>
-                <label>Taste JavaScript</label>
-                <button class="destroy"></button>
+                <input 
+                    class="toggle" 
+                    type="checkbox" 
+                    ${todo.completed && 'checked'}
+                    onchange="dispatch('toggle', ${index})"
+                >
+                <label ondblclick="dispatch('startEdit', ${index})">${todo.title}</label>
+                <button class="destroy" onclick="dispatch('destroy', ${index})"></button>
             </div>
-            <input class="edit" value="Create a TodoMVC template">
+            <input 
+                class="edit" 
+                value="${todo.title}"
+                onkeyup="event.keyCode === 13 && dispatch('endEdit', this.value.trim()) ||
+                    event.keyCode === 27 && dispatch('cancelEdit')"
+                onblue="dispatch('endEdit', this.value.trim())"
+            >
         </li>
     `
 }
 
-export default TodoItem
+export default connect()(TodoItem)
--- component/TodoList.js
+++ component/TodoList.js
@@ -1,16 +1,28 @@
 import html from "../core.js";
+import {connect} from "../store.js"
 import TodoItem from "./TodoItem.js";
 
-function TodoList() {
+function TodoList({ todos, filter, filters }) {
+    // console.log(todos)
+    // console.log(filters)
     return html`
         <section class="main">
-            <input id="toggle-all" class="toggle-all" type="checkbox">
+            <input 
+                id="toggle-all" 
+                class="toggle-all" 
+                type="checkbox"
+                onchange="dispatch('toggleAll', this.checked)"
+                ${todos.every(filters.completed) && 'checked'}
+            >
             <label for="toggle-all">Mark all as complete</label>
             <ul class="todo-list">
-                ${TodoItem()}
+                ${todos
+                    .filter(filters[filter])
+                    .map((todo, index) => TodoItem({ todo, index }))
+                }
             </ul>
         </section>
     `
 }
 
-export default TodoList
+export default connect()(TodoList)
